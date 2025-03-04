3b4416ced10cf72058b066fcf7ce0b097bad33b1
Daniele De Matteo <daniele@kuama.net>
2025-03-04 10:49:04 +0100

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+using ConsoleApp;
+
+var arguments = Parser.Default.ParseArguments<ProgramArgumentsReadDto>(args)?.Value;
+if (arguments == null)
+{
+    throw new Exception("[DEV] could not parse arguments");
+}
+
+Console.WriteLine($"flavor: {arguments.Flavor}");
+Console.WriteLine($"environment: {arguments.Environment}");
+Console.WriteLine($"version: {arguments.Version}");
---
+++ ConsoleApp/ProgramArgumentsReadDto.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace ConsoleApp;
+
+public record ProgramArgumentsReadDto
+{
+    [Option('f', "flavor", Required = true, HelpText = "Which flavor of the artifact should be created by the setup")]
+    public required string Flavor { get; init; }
+    
+    [Option('e', "environment", Required = true, HelpText = "Whether the final installed artifact should point to staging or production apis, and whether the final artifact should be uploaded to staging or production backoffice")]
+    public required string Environment { get; init; }
+    
+    [Option('v', "version", Required = true, HelpText = "The sem-ver of the artifact. Will also be appended to the setup filename")]
+    public required string Version { get; init; }
+}
